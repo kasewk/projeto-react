76aa98cdc2f5a899ae87956a61ae9d5223137cf9
kasewk <phpaes99@gmail.com>
2021-09-10 18:50:17 -0300

    Estruturando projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+
+// Pages
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Comany";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
+
 function App() {
-  return (
-    <div>Costs</div>
-  );
+    return (
+
+        <BrowserRouter>
+            <ul>
+                <Link to="/">Home</Link>
+                <Link to="/contact">Contato</Link>
+                <Link to="/company">Empresa</Link>
+                <Link to="/newproject">Novo projeto</Link>
+            </ul>
+            <Switch>
+                <Container>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/company">
+                        <Company />
+                    </Route>
+                    <Route exact path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route exact path="/newproject">
+                        <NewProject />
+                    </Route>
+                </Container>
+            </Switch>
+            <p>Footer</p>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/layout/Container.jsx
@@ -0,0 +1,7 @@
+import styles from './Container.module.css'
+
+export default function Container({ children }) {
+    return (
+        <div>{children}</div>
+    )
+}
